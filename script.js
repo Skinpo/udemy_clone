28261ac6053b74d66386be8f83b5fd3d56ba3d3c
celestine <lestine603@gmail.com>
2023-01-11 13:41:14 +0100

    Almost done

--- script.js
+++ script.js
@@ -43,3 +43,36 @@ tabs.forEach((tab) => {
         });
     });
 })
+
+
+// const prev = document.querySelector(".prev");
+// const next = document.querySelector(".next");
+// const carousel = document.querySelector(".carousel-container");
+// const track = document.querySelector(".track");
+// let width = carousel.offsetWidth;
+// let index = 0;
+// window.addEventListener("resize", function () {
+//   width = carousel.offsetWidth;
+// });
+// next.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   index = index + 1.01;
+//   prev.className.includes("display");
+//   prev.display = "show";
+// //   prev.classList.contains("display");
+//   console.log(prev.display);
+// //   prev.classList.display = "show";
+  
+//   track.style.transform = "translateX(" + index * -width + "px)";
+//   if (track.offsetWidth - index * width < index * width) {
+//     next.classList.add("hide");
+//   }
+// });
+// prev.addEventListener("click", function () {
+//   index = index - 1;
+//   next.classList.remove("hide");
+//   if (index === 0) {
+//     prev.classList.remove("show");
+//   }
+//   track.style.transform = "translateX(" + index * -width + "px)";
+// });
